edd77c81de563864de34335f593176e29758b187
Felipe Gasper <felipe@felipegasper.com>
2024-05-17 19:43:23 -0400

    bison again

--- extern-post.js
+++ extern-post.js
@@ -8,21 +8,23 @@ if (!wrappedGawk) {
     gawk = async () => {
         const module = await origGawk();
 
+        // GAWK’s main() doesn’t clean up after itself. To accommodate that,
+        // we take a snapshot of WASM’s memory before the first invocation
+        // then reset it before each subsequent invocation.
         let stableMemoryView;
 
         return function(input, progText) {
-            if (!stableMemoryView) {
+            const memoryView = new Uint8Array(module.asm.memory.buffer);
+
+            if (stableMemoryView) {
+                memoryView.set(stableMemoryView);
+            } else {
                 stableMemoryView = new Uint8Array(
                     module.asm.memory.buffer.slice(),
                 );
             }
 
-            const got = module.run(input, progText);
-
-            const postMemoryView = new Uint8Array(module.asm.memory.buffer);
-            postMemoryView.set(stableMemoryView);
-
-            return got;
+            return module.run(input, progText);
         };
     };
 
